e8c0b651a62429f5c9f2cac216795c405322044f
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-12 20:50:04 +0200

    update

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -14,13 +14,15 @@ export default function VanDetail() {
     const location = useLocation();
 
     const search = location.state?.search || "";
+    const type = location.state?.type || "all";
+    
     return (
         <div className="van-detail-container">
             <Link
                 to={`../${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
